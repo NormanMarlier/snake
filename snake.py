3246c67e0dcf465aeed3f0fca06b2d073583a351
Norman <norman.marlier@gmail.com>
2024-03-15 14:42:10 +0100

    Add toroidal space

--- snake.py
+++ snake.py
@@ -1,18 +1,16 @@
 import pygame
-import sys,random
-import enum
+import sys, random
 from pygame.math import Vector2
 
 
 # Constants
-CELL_SIZE = 20
-CELL_NUMBER = 40
+CELL_SIZE = 40
+CELL_NUMBER = 20
+WIDTH = CELL_SIZE * CELL_NUMBER
+HEIGHT = CELL_SIZE * CELL_NUMBER
+FONTSIZE = 25
+FRAMERATE = 60
 
-class Ori(enum.Enum):
-	UP = 0
-	DOWN = 1
-	LEFT = 2
-	RIGHT = 3
 
 class FRUIT:
 	def __init__(self):
@@ -31,9 +29,7 @@ class FRUIT:
 
 class SNAKE:
 	def __init__(self):
-		self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]
-		self.ori = [Ori.RIGHT, Ori.RIGHT, Ori.RIGHT]
-		self.direction = Vector2(0, 0)
+		self.reset()
 		self.new_block = False
 
 		self.head_up = pygame.image.load('Graphics/head_up.png').convert_alpha()
@@ -69,21 +65,39 @@ class SNAKE:
 			elif index == len(self.body) - 1:
 				screen.blit(self.tail, block_rect)
 			else:
-				previous_block = self.body[index + 1] - block
-				next_block = self.body[index - 1] - block
-				if previous_block.x == next_block.x:
-					screen.blit(self.body_vertical,block_rect)
-				elif previous_block.y == next_block.y:
-					screen.blit(self.body_horizontal,block_rect)
+				# Direction between i+1 and i block
+				previous_dir = block - self.body[index + 1]
+				# Direction between i-1 and i block
+				next_dir = self.body[index - 1] - block
+				# Same horizontal component
+				if (previous_dir.y == 0) and (next_dir.y == 0):
+					screen.blit(self.body_horizontal, block_rect)
+				# Same vertical component
+				elif (previous_dir.x == 0) and (next_dir.x == 0):
+					screen.blit(self.body_vertical, block_rect)
 				else:
-					if previous_block.x == -1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == -1:
-						screen.blit(self.body_tl,block_rect)
-					elif previous_block.x == -1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == -1:
-						screen.blit(self.body_bl,block_rect)
-					elif previous_block.x == 1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == 1:
-						screen.blit(self.body_tr,block_rect)
-					elif previous_block.x == 1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == 1:
-						screen.blit(self.body_br,block_rect)
+					# It exists 4 cases
+					# 1 : next up and previous right or next left and previous down
+					# 2 : next up and previous left or next right and previous down
+					# 3 : next down and previous right or next left and previous up
+					# 4 : next down and previous left or next right and previous up
+					next_up = (next_dir.x == 0) and ((next_dir.y < 0) or (next_dir.y == CELL_NUMBER - 1))
+					next_down = (next_dir.x == 0) and ((next_dir.y > 0) or (next_dir.y == -CELL_NUMBER + 1))
+					next_right = (next_dir.y == 0) and ((next_dir.x > 0) or (next_dir.x == -CELL_NUMBER + 1))
+					next_left = (next_dir.y == 0) and ((next_dir.x < 0) or (next_dir.x == CELL_NUMBER - 1))
+					previous_up = (previous_dir.x == 0) and ((previous_dir.y < 0) or (previous_dir.y == CELL_NUMBER - 1))
+					previous_down = (previous_dir.x == 0) and ((previous_dir.y > 0) or (previous_dir.y == -CELL_NUMBER + 1))
+					previous_right = (previous_dir.y == 0) and ((previous_dir.x > 0) or (previous_dir.x == -CELL_NUMBER + 1))
+					previous_left = (previous_dir.y == 0) and ((previous_dir.x < 0) or (previous_dir.x == CELL_NUMBER - 1))
+					if (next_up and previous_left) or (next_right and previous_down):
+						screen.blit(self.body_tr, block_rect)
+					elif (next_up and previous_right) or (next_left and previous_down):
+						screen.blit(self.body_tl, block_rect)
+					elif (next_down and previous_left) or (next_right and previous_up):
+						screen.blit(self.body_br, block_rect)
+					elif (next_down and previous_right) or (next_left and previous_up):
+						screen.blit(self.body_bl, block_rect)
+
 
 	def update_head_graphics(self):
 		head_relation = self.body[1] - self.body[0]
@@ -99,16 +113,42 @@ class SNAKE:
 		elif tail_relation == Vector2(0, 1): self.tail = self.tail_up
 		elif tail_relation == Vector2(0, -1): self.tail = self.tail_down
 
+	@staticmethod
+	def check_coordinate(coord):
+		return coord % CELL_NUMBER
+	
+	def update_direction(self, start, dir):
+		# Update the direction
+		new_head = start + dir
+		checked_x = self.check_coordinate(new_head.x)
+		checked_y = self.check_coordinate(new_head.y)
+		new_head.update(checked_x, checked_y)
+		return new_head
+		
+	def check_direction(self, dir):
+		return self.update_direction(self.body[0], dir) != self.body[1]
+
 	def move_snake(self):
 		if self.new_block == True:
 			body_copy = self.body[:]
-			body_copy.insert(0,body_copy[0] + self.direction)
-			self.body = body_copy[:]
-			self.new_block = False
 		else:
 			body_copy = self.body[:-1]
-			body_copy.insert(0,body_copy[0] + self.direction)
-			self.body = body_copy[:]
+		new_head = self.update_direction(self.body[0], self._direction)
+		body_copy.insert(0, new_head)
+		self.body = body_copy[:]
+		self.new_block = False
+	
+	@property
+	def direction(self):
+		return self._direction
+	
+	@direction.setter
+	def direction(self, dir):
+		# Has to be a Vector2
+		assert type(dir) == Vector2
+		# Cannot go into the body
+		if self.check_direction(dir):
+			self._direction = dir
 
 	def add_block(self):
 		self.new_block = True
@@ -118,20 +158,23 @@ class SNAKE:
 
 	def reset(self):
 		self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]
-		self.direction = Vector2(0, 0)
+		self._direction = Vector2(0, 0)
 
 
 class MAIN:
 	def __init__(self):
 		self.snake = SNAKE()
 		self.fruit = FRUIT()
+		self.score = 0
 
 	def update(self):
 		self.snake.move_snake()
 		self.check_collision()
 		self.check_fail()
+		self.update_score()
 
 	def draw_elements(self, screen):
+		screen.fill((175, 215, 70))
 		self.draw_grass(screen)
 		self.fruit.draw_fruit(screen)
 		self.snake.draw_snake(screen)
@@ -153,13 +196,24 @@ class MAIN:
 		
 	def game_over(self):
 		self.snake.reset()
+	
+	def update_score(self):
+		if self.snake.direction != Vector2(0, 0):
+			self.score = len(self.snake.body) - 3
 
 	def draw_grass(self, screen):
 		grass_color = (167, 209, 61)
-		
+		for row in range(CELL_NUMBER):
+			for col in range(CELL_NUMBER):
+				if (row % 2 == 0) and (col % 2 == 0):
+					grass_rect = pygame.Rect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+					pygame.draw.rect(screen, grass_color, grass_rect)
+				if (row % 2 != 0) and (col % 2 != 0):
+					grass_rect = pygame.Rect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+					pygame.draw.rect(screen, grass_color, grass_rect)
 
 	def draw_score(self, screen):
-		score_text = str(len(self.snake.body) - 3)
+		score_text = str(self.score)
 		score_surface = GAME_FONT.render(score_text, True, (56, 74, 12))
 		score_x = int(CELL_SIZE * CELL_NUMBER - 60)
 		score_y = int(CELL_SIZE * CELL_NUMBER - 40)
@@ -174,11 +228,11 @@ class MAIN:
 
 pygame.mixer.pre_init(44100, -16, 2, 512)
 pygame.init()
-screen = pygame.display.set_mode((CELL_NUMBER * CELL_SIZE, CELL_NUMBER * CELL_SIZE))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
-GAME_FONT = pygame.font.Font('Font/PoetsenOne-Regular.ttf', 25)
+GAME_FONT = pygame.font.Font('Font/PoetsenOne-Regular.ttf', FONTSIZE)
 SCREEN_UPDATE = pygame.USEREVENT
-pygame.time.set_timer(SCREEN_UPDATE, 150)
+pygame.time.set_timer(SCREEN_UPDATE, 140)
 
 main_game = MAIN()
 run = True
@@ -199,7 +253,7 @@ while run:
             if event.key == pygame.K_LEFT:
                 main_game.snake.direction = Vector2(-1, 0)
 
-    screen.fill((175, 215, 70))
+    
     main_game.draw_elements(screen)
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(FRAMERATE)
